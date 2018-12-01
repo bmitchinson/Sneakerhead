52b4efd37c23ba9a0b04407e28bf01d29d6e4bd5
Melanie King <melanie-king@uiowa.edu>
2018-12-01 16:10:43 -0600

    wrapper class, establish database connection -Mel

--- src/Wrapper.java
+++ src/Wrapper.java
@@ -1,13 +1,15 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Wrapper {
 
     public static void main(String[] args) {
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://s-l112.engr.uiowa.edu/phpMyAdmin/db_structure.php?server=1&db=engr_class037", "engr_class037", "smb123");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://s-l112.engr.uiowa.edu", "engr_class037", "mbs123");
             System.out.println("Connection Established.");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT Password FROM Users");
+            System.out.println(resultSet.getObject(0));
+
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
