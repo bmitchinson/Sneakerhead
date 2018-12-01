13141f20f4f89fdde5e737c608b0f10e74862619
sammorgan <samuel-morgan@uiowa.edu>
2018-12-01 01:00:58 -0600

    Started HomeFrame - need to implement ItemTiles then finishing touches on size of window/layout can be completed
    Started LoginFrame
    - need to implement wrapper to properly implement functionality for login and register buttons
    
    Started Item
    - need to confirm how we want Shoe Size returned
    - need to decide whether to name is item or shoe
    - need to create ItemViewFrame so the getItemView method functions
    - need to create ItemTile so the getItemTile method functions

--- src/HomeFrame.java
+++ src/HomeFrame.java
@@ -1,17 +1,77 @@
-import com.oracle.xmlns.internal.webservices.jaxws_databinding.ExistingAnnotationsType;
-
 import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
 
 public class HomeFrame extends JFrame {
-    public HomeFrame(){
+    private final JButton sellButton;
+    private final JButton loginButton;
+    private final JPanel itemPanel;
+    private final JScrollPane scrollPane;
 
+
+    public HomeFrame(){
+        sellButton = new JButton("Sell Item");
+        sellButton.setMinimumSize(new Dimension(100,25));
+        loginButton = new JButton("Login");
+        loginButton.setMinimumSize(new Dimension(75,25));
+
+        JPanel topPanel = new JPanel();
+        topPanel.setMaximumSize(new Dimension(500,50));
+        topPanel.setLayout(new BoxLayout(topPanel,BoxLayout.LINE_AXIS));
+        topPanel.add(Box.createHorizontalStrut(5));
+        topPanel.add(sellButton);
+        topPanel.add(Box.createHorizontalGlue());
+        topPanel.add(loginButton);
+        topPanel.add(Box.createHorizontalStrut(5));
+
+        itemPanel = new JPanel();
+        initializeItemPanel();
+
+        scrollPane = new JScrollPane(itemPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setMaximumSize(new Dimension(480,510));
+        scrollPane.setAlignmentX(CENTER_ALIGNMENT);
+        
+
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
+
+        add(Box.createVerticalStrut(10));
+        add(topPanel);
+        add(Box.createVerticalStrut(15));
+        add(scrollPane);
+        add(Box.createVerticalStrut(15));
+        
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setSize(600,600);
+        this.setSize(500,600);
         this.setResizable(false);
         this.setVisible(true);
+    }
+
+    //TODO: Get Items from wrapper and add them to the JPanel ItemPanel. Item Panel is then added to scrollPane
+    //TODO: Remove temporary panels used for initializing after a set size for ItemPanel is determined
+    private void initializeItemPanel(){
+        JPanel[] panels = new JPanel[5];
+
+        for(int i=0; i<panels.length; i++){
+            Random random = new Random();
+            int r = random.nextInt(256);
+            int g = random.nextInt(256);
+            int b = random.nextInt(256);
+
+            panels[i] = new JPanel();
+            panels[i].setPreferredSize(new Dimension(480,170));
+            panels[i].setBackground(new Color(r,g,b));
+        }
+
+        itemPanel.setPreferredSize(new Dimension(460,170 * panels.length));
+        itemPanel.setLayout(new GridLayout(panels.length,1));
+        for(int i=0; i<panels.length; i++){
+            itemPanel.add(panels[i]);
+        }
     }
 
     public static void main(String[] args){
         HomeFrame frame = new HomeFrame();
     }
 }
+
+
--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,65 @@
+import javax.swing.*;
+
 public class Item {
+    private String name;
+    private String description;
+    private String brand;
+    private String condition;
+    private Integer size;
+    private String color;
+    private String gender;
+
+    public Item(String name, String description, String brand, String condition, String color, String gender, Integer size){
+        this.name = name;
+        this.description = description;
+        this.brand = brand;
+        this.condition = condition;
+        this.size = size;
+        this.color = color;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    //TODO: Verify how we want size returned from the getSize method
+    public String getSize() {
+        return size.toString();
+    }
+
+    //TODO: Create an ItemTile Class that shows a condensed version of the Item's data in a (JPanel)
+    public JPanel getItemTile(){
+        //ItemTile tile = new ItemTile(this);
+        //return tile;
+        return null;
+    }
+
+    //TODO: Create an ItemWindow Class that shows the item in its own JFrame
+    public JFrame getItemWindow(){
+        //ItemWindow window = new ItemWindow(this);
+        //return window;
+        return null;
+    }
+
 }
