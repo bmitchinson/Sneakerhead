e4d7df3f166e8ef30a3a550a528baa649add5406
Ben Mitchinson <benjamin-mitchinson@uiowa.edu>
2018-12-01 17:07:46 -0600

    Beginning Wrapper Work

--- src/Server.java
+++ src/Server.java
@@ -14,8 +14,6 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-// TODO: Need to fill in comments with this
-
 public class Server {
 
     // Networking
--- src/Wrapper.java
+++ src/Wrapper.java
@@ -2,20 +2,47 @@ import java.sql.*;
 
 public class Wrapper {
 
-    public static void main(String[] args) {
+    private Connection connection;
+
+    public Wrapper(){
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://s-l112.engr.uiowa.edu/engr_class037", "engr_class037", "mbs123");
-            System.out.println("Connection Established.");
-            Statement statement = connection.createStatement();
-            System.out.println("Statement out of connection");
-            ResultSet resultSet = statement.executeQuery("SELECT Password FROM Users");
-
-            while(resultSet.next()){
-                System.out.println(resultSet.getString(1));
-            }
-
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
+            this.connection = DriverManager.getConnection("jdbc:mysql://s-l112.engr.uiowa.edu/engr_class037", "engr_class037", "mbs123");
+        } catch (SQLException e) {
+            System.out.println("Unable to connect to remote database");
+            e.printStackTrace();
         }
     }
+
+    public Item getItemInfo(int id){
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery("")
+
+    }
+
+    /*try {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://s-l112.engr.uiowa.edu/engr_class037", "engr_class037", "mbs123");
+        System.out.println("Connection Established.");
+        Statement statement = connection.createStatement();
+        System.out.println("Statement out of connection");
+        ResultSet resultSet = statement.executeQuery("SELECT Password FROM Users");
+
+        while(resultSet.next()){
+            System.out.println(resultSet.getString(1));
+        }
+
+    } catch (SQLException sqlException) {
+        sqlException.printStackTrace();
+    }*/
+
+    /*
+    TODO: Methods
+    getItemInfo(ID)
+    getAllItems() - Uses get getItemInfo and returns an item array
+    buyItem(ID)
+    sellItem(Item)
+    createUser(Name, Pass, Type (123))
+    login(name, pass) - true or false, telling gui to use provided username in corner
+    getUserType(name) - return string of user type
+
+     */
 }
