fbff8fb1fe01975c81d51a8f0697112e1c3be401
Melanie King <melanie-king@uiowa.edu>
2018-12-01 15:44:34 -0600

    update'

--- src/Wrapper.java
+++ src/Wrapper.java
@@ -1,3 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Wrapper {
 
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://s-l112.engr.uiowa.edu/phpMyAdmin/db_structure.php?server=1&db=engr_class037", "engr_class037", "smb123");
+            System.out.println("Connection Established.");
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
 }
