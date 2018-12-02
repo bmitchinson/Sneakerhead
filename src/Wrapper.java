75483104ebeae2e68f2706eb184a601d8eb026f1
Ben Mitchinson <benjamin-mitchinson@uiowa.edu>
2018-12-01 20:32:20 -0600

    Working getItemInfo()

--- src/Wrapper.java
+++ src/Wrapper.java
@@ -20,30 +20,15 @@ public class Wrapper {
             ResultSet result =
                     statement.executeQuery("SELECT * FROM Items where ID = " + id);
 
-            ResultSetMetaData rsmd = result.getMetaData();
-            int columnsNumber = rsmd.getColumnCount();
+            result.next();
 
-            while (result.next()) {
-                for (int i = 1; i <= 11; i++) {
-                    if (i > 1) System.out.print(",  ");
-                    System.out.print(result.getString(i));
-                }
-                System.out.println("");
+            for (int i = 1; i <= 11; i++) {
+                if (i > 1) System.out.print(",  ");
+                System.out.print(result.getString(i));
             }
+            System.out.println("");
 
-            /*Item test = new Item(result.getInt(0),
-                    result.getString(1),
-                    result.getString(2),
-                    result.getString(3),
-                    result.getString(4),
-                    result.getString(5),
-                    result.getString(6),
-                    result.getFloat(7),
-                    result.getDouble(8),
-                    result.getInt(9),
-                    result.getString(10));*/
-
-            /*Item test = new Item(result.getInt("ID"),
+            Item test = new Item(result.getInt("ID"),
                     result.getString("Name"),
                     result.getString("Description"),
                     result.getString("Brand"),
@@ -53,10 +38,10 @@ public class Wrapper {
                     result.getFloat("Size"),
                     result.getDouble("Price"),
                     result.getInt("Quantity"),
-                    result.getString("URL"));*/
+                    result.getString("URL"));
 
-            //System.out.println(test);
-            return null;
+            System.out.println(test);
+            return test;
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -66,21 +51,6 @@ public class Wrapper {
         return null;
     }
 
-    /*try {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://s-l112.engr.uiowa.edu/engr_class037", "engr_class037", "mbs123");
-        System.out.println("Connection Established.");
-        Statement statement = connection.createStatement();
-        System.out.println("Statement out of connection");
-        ResultSet resultSet = statement.executeQuery("SELECT Password FROM Users");
-
-        while(resultSet.next()){
-            System.out.println(resultSet.getString(1));
-        }
-
-    } catch (SQLException sqlException) {
-        sqlException.printStackTrace();
-    }*/
-
     public static void main(String[] args) {
         Wrapper wrapper = new Wrapper();
         Item test = wrapper.getItemInfo(2);
@@ -89,7 +59,6 @@ public class Wrapper {
 
     /*
     TODO: Methods
-    getItemInfo(ID)
     getAllItems() - Uses get getItemInfo and returns an item array
     buyItem(ID)
     sellItem(Item)
